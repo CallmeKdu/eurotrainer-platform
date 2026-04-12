ef9110436fe639b6f0021c70ed51f20e0ca8e27d
callmekdu <carlos.1501.freire@gmail.com>
2026-04-12 17:04:54 -0300

    feat: inicializa firebase core e gera os configs de projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Importe isso
+import 'firebase_options.dart'; // E isso
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(const MyApp());
 }
 
