cceb0f96568c46002c57d3839381dec2a91f8387
callmekdu <carlos.1501.freire@gmail.com>
2026-05-01 16:49:22 -0300

    feat: atualizando a homepage

--- functions/src/index.ts
+++
@@ -1,4 +0,0 @@
-import * as functions from "firebase-functions/v2/https";
-
-// As funções generate2fa e verify2fa foram removidas pois agora
-// o fluxo é controlado pelo SDK do Firebase Auth no app Flutter.
